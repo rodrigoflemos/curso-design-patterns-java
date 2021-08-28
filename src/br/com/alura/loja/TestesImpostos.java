c7e2b2728fd790ef834a479c1deef6a673dfa4c3
Rodrigo Lemos <rodrigoflemos@gmail.com>
2021-08-28 14:58:40 -0300

    Chain of Responsability + Template Method

--- src/br/com/alura/loja/TestesImpostos.java
+++ src/br/com/alura/loja/TestesImpostos.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja;
 
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.ICMS;
 import br.com.alura.loja.imposto.ISS;
@@ -10,10 +11,16 @@ import java.math.BigDecimal;
 public class TestesImpostos {
 
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        Orcamento orcamentoImposto = new Orcamento(new BigDecimal("100"), 1);
         CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
 
-        System.out.println(calculadoraDeImpostos.calcular(orcamento, new ICMS()));
-        System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS()));
+        System.out.println(calculadoraDeImpostos.calcular(orcamentoImposto, new ICMS()));
+        System.out.println(calculadoraDeImpostos.calcular(orcamentoImposto, new ISS()));
+
+        Orcamento orcamentoDesconto = new Orcamento(new BigDecimal("200"), 6);
+        Orcamento orcamentoDesconto2 = new Orcamento(new BigDecimal("1000"), 1);
+        CalculadoraDeDescontos calculadoraDeDescontos = new CalculadoraDeDescontos();
+        System.out.println(calculadoraDeDescontos.calcular(orcamentoDesconto));
+        System.out.println(calculadoraDeDescontos.calcular(orcamentoDesconto2));
     }
 }
---
+++ src/br/com/alura/loja/desconto/DescontoParaOrcamentoComMaisdeCincoItens.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja.desconto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class DescontoParaOrcamentoComMaisdeCincoItens extends Desconto {
+
+    public DescontoParaOrcamentoComMaisdeCincoItens(Desconto proximo) {
+        super(proximo);
+    }
+
+    public BigDecimal efetuarCalculo(Orcamento orcamento) {
+        return orcamento.getValor().multiply(new BigDecimal("0.1"));
+    }
+
+    @Override
+    public boolean deveAplicar(Orcamento orcamento) {
+        return orcamento.getQuantidadeItens() > 5;
+    }
+}
